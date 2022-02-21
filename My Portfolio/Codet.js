977bf28c63d174ef1fdecfb62cd26e5e047c7643
Cleveraccountant96 <97385389+Cleveraccountant96@users.noreply.github.com>
2022-02-21 08:03:48 +0000

    Musab Portfolio for Week 6

---
+++ My Portfolio/Codet.js
@@ -0,0 +1,29 @@
+function validateForm(){
+    let x = document.forms["myForm"]["fname"].value;
+
+    if(x == ""){
+        alert("Name Must be Filled");
+        return false;
+    }
+}
+function validateForm(){
+    let x = document.forms["myForm"]["lname"].value;
+
+    if(x == ""){
+        alert("Surname Must be Filled");
+        return false;
+    }
+}
+function validateForm(){
+    let x = document.forms["myForm"]["email"].value;
+
+    if(x == ""){
+        alert("Invalid Email");
+        return false;
+    }
+    else["submit"]
+        alert("Valid Email");
+        return true;
+    }
+    onclick(ok)
+
